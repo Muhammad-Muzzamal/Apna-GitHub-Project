3b56473ab26552047d442e7113fecbb1cf7b3646
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2026-07-31 15:36:09 +0500

    create model of User, Repository, and Issue

---
+++ backend/models/repo.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const RepoSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    content: [
+        {
+            type: String
+        }
+    ],
+    visibility: {
+        type: Boolean,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        reqired: true
+    },
+    issues: [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Issue"
+        }
+    ]
+
+});
+
+const Repo = mongoose.Model("Repo", RepoSchema);
+module.exports = Repo;
