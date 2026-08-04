6e0261ff7fa89600bd812d56369e976f6a092d75
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2026-08-04 20:06:51 +0500

    Auth Context and store token in local storage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
+import "./index.css";
+
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>,
 );
